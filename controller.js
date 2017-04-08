8603f078c21bad7f37d247dd7a1af4af95002206
Niranjan-B <nero.niranjan@gmail.com>
2017-04-08 16:06:48 -0700

    previous and next button functionality on users tab only.

--- controller.js
+++ controller.js
@@ -4,7 +4,18 @@ app.controller('controlNinja', function($scope, $http) {
     // change the way tooltip looks later on
     $scope.isTooltipOpen = false;
 
-    //$scope.showUserTable = "false";
+    $scope.hideUserTable = true;
+    $scope.hidePagesTable = true;
+    $scope.hideEventsTable = true;
+    $scope.hidePlacesTable = true;
+    $scope.hideGroupsTable = true;
+    $scope.hideFavoritesTable = true;
+
+    $scope.hideNextButtonUsersTab = true;
+    $scope.hidePreviousButtonUsersTab = true;
+
+    var nextPageUrlUsersTab;
+    var previousPageUrlUsersTab;
     
     $scope.submit = function() {
         if ($scope.query) {
@@ -14,13 +25,29 @@ app.controller('controlNinja', function($scope, $http) {
             $http.get("https://php-gae-161219.appspot.com/?search_type=users&searched_keyword="+$scope.query ,
                      { headers: { 'Access-Control-Allow-Origin':'*' } })
                  .then(function (response) {
+                    $scope.hideUserTable = false;
                     $scope.users = response.data.data;
+
+                    if (response.data.paging.next != null) {
+                        nextPageUrlUsersTab = response.data.paging.next;
+                        $scope.hideNextButtonUsersTab = false;
+                    } else {
+                        $scope.hideNextButtonUsersTab = true;
+                    }
+                    if (response.data.paging.previous != null) {
+                        previousPageUrlUsersTab = response.data.paging.previous;
+                        $scope.hidePreviousButtonUsersTab = false;
+                    } else {
+                        $scope.hidePreviousButtonUsersTab = true;
+                    }
+
                  }, function (error){
                     console.log(error);
                  });
             $http.get("https://php-gae-161219.appspot.com/?search_type=pages&searched_keyword="+$scope.query ,
                      { headers: { 'Access-Control-Allow-Origin':'*' } })
                  .then(function (response) {
+                    $scope.hidePagesTable = false;
                     $scope.pages = response.data.data;
                  }, function (error){
                     console.log(error);
@@ -28,6 +55,7 @@ app.controller('controlNinja', function($scope, $http) {
             $http.get("https://php-gae-161219.appspot.com/?search_type=events&searched_keyword="+$scope.query ,
                      { headers: { 'Access-Control-Allow-Origin':'*' } })
                  .then(function (response) {
+                    $scope.hideEventsTable = false;
                     $scope.events = response.data.data;
                  }, function (error){
                     console.log(error);
@@ -35,6 +63,7 @@ app.controller('controlNinja', function($scope, $http) {
             $http.get("https://php-gae-161219.appspot.com/?search_type=places&searched_keyword="+$scope.query ,
                      { headers: { 'Access-Control-Allow-Origin':'*' } })
                  .then(function (response) {
+                    $scope.hidePlacesTable = false;
                     $scope.places = response.data.data;
                  }, function (error){
                     console.log(error);
@@ -42,6 +71,7 @@ app.controller('controlNinja', function($scope, $http) {
             $http.get("https://php-gae-161219.appspot.com/?search_type=groups&searched_keyword="+$scope.query ,
                      { headers: { 'Access-Control-Allow-Origin':'*' } })
                  .then(function (response) {
+                    $scope.hideGroupsTable = false;
                     $scope.groups = response.data.data;
                  }, function (error){
                     console.log(error);
@@ -52,4 +82,52 @@ app.controller('controlNinja', function($scope, $http) {
             $scope.isTooltipOpen = true;
         }
     }
+
+    $scope.onNextButtonClickedUsersPage = function() {
+        $http.get(nextPageUrlUsersTab , { headers: { 'Access-Control-Allow-Origin':'*' } })
+                 .then(function (response) {
+                    $scope.hideUserTable = false;
+                    $scope.users = response.data.data;
+
+                    if (response.data.paging.next != null) {
+                        nextPageUrlUsersTab = response.data.paging.next;
+                        $scope.hideNextButtonUsersTab = false;
+                    } else {
+                        $scope.hideNextButtonUsersTab = true;
+                    }
+                    if (response.data.paging.previous != null) {
+                        previousPageUrlUsersTab = response.data.paging.previous;
+                        $scope.hidePreviousButtonUsersTab = false;
+                    } else {
+                        $scope.hidePreviousButtonUsersTab = true;
+                    }
+
+                 }, function (error){
+                    console.log(error);
+                 });   
+    }
+    $scope.onPreviousButtonClickedUsersPage = function() {
+        $http.get(previousPageUrlUsersTab , { headers: { 'Access-Control-Allow-Origin':'*' } })
+                 .then(function (response) {
+                    $scope.hideUserTable = false;
+                    $scope.users = response.data.data;
+
+                    if (response.data.paging.next != null) {
+                        nextPageUrlUsersTab = response.data.paging.next;
+                        $scope.hideNextButtonUsersTab = false;
+                    } else {
+                        $scope.hideNextButtonUsersTab = true;
+                    }
+                    if (response.data.paging.previous != null) {
+                        previousPageUrlUsersTab = response.data.paging.previous;
+                        $scope.hidePreviousButtonUsersTab = false;
+                    } else {
+                        $scope.hidePreviousButtonUsersTab = true;
+                    }
+
+                 }, function (error){
+                    console.log(error);
+                 });   
+    }
+
 });
